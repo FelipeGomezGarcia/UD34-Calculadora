d10b9cbdb1a6537140847c42f1cee9e188281309
FelipeGomezGarcia <feli.goga@gmail.com>
2022-09-06 19:20:10 +0200

    Calculadora terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UD34-Calculadora';
+
+  operacion: string="";
+  resultado = 0;
+  numeros: any = [];
+
+  escribirInput(numero: string){
+    this.operacion += numero;
+  }
+
+  borrarInput(){
+    this.operacion = this.operacion.substring(0, this.operacion.length-1);
+  }
+
+  darResultado(){
+    this.resultado = eval(this.operacion);
+  }
+
 }
